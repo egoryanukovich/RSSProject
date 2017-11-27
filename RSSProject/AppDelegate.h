7ccc0e7e7ce6f481ccb3ea15b40b798b661b0c42
egoryanukovich <drummlover@tut.by>
2017-11-27 11:17:48 +0300

    Initial Commit

---
+++ RSSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSSProject
+//
+//  Created by Egor Yanukovich on 11/27/17.
+//  Copyright © 2017 Egor Yanukovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSProject
+//
+//  Created by Egor Yanukovich on 11/27/17.
+//  Copyright © 2017 Egor Yanukovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
